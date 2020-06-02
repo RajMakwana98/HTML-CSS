ce6220073ab59fa6d1884adf20ff85a6a33de4c4
Raj Makwana <rajmakwana98@gmail.com>
2020-06-02 12:34:20 +0530

    User Added Functionality
    Fetch Random User and user with money display on  the Website

--- DOM Array Method(map,forEach,sort,Reduce,Filter)/script.js
+++ DOM Array Method(map,forEach,sort,Reduce,Filter)/script.js
@@ -6,12 +6,12 @@ const calculate_wealthBtn = document.getElementById("calculate_wealth");
 
 let data = [];
 
-getrandomUser();
-getrandomUser();
-getrandomUser();
+// getRandomUser();
+// getRandomUser();
+// getRandomUser();
 
 //Fetch Random user and add Money
-async function getrandomUser() {
+async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
 
@@ -19,8 +19,37 @@ async function getrandomUser() {
 
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
-    Money: Math.floor(Math.random() * 1000000),
+    money: Math.floor(Math.random() * 1000000),
   };
 
-  console.log(newUser);
+  addData(newUser);
 }
+
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+//Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+//Format number as money
+function formatMoney(number) {
+  return "₹" + number.toFixed(2).replace(/(\d)(?=(\d{2})+\d\.)/g, "$1,");
+}
+
+//Event Listeners
+add_userBtn.addEventListener("click", getRandomUser);
