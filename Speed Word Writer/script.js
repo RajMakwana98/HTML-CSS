b4ba4c425fb1beaf971574aa9e4ea12b305211e5
Raj Makwana <rajmakwana98@gmail.com>
2020-06-07 18:28:46 +0530

    Time & Difficulty set.
    Update with time as per the difficulty(level) select.

--- Speed Word Writer/script.js
+++ Speed Word Writer/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById("word");
 const text = document.getElementById("text");
 const scoreEl = document.getElementById("score");
 const timeEl = document.getElementById("time");
-const endGameEl = document.getElementById("end-game");
+const endGameEl = document.getElementById("end-game-container");
 const settingBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
@@ -45,6 +45,25 @@ let score = 0;
 //  Init  time
 let time = 10;
 
+// set difficulty to value in medium
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+// set difficulty value
+
+difficultySelect.value =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+// focus on the text on start
+text.focus();
+
+// start counting time
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -62,9 +81,31 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+//Update Time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    //endgame
+    gameOver();
+  }
+}
+
+// game over, show end screen
+function gameOver() {
+  endGameEl.innerHTML = `<h1>Time Over</h1>
+  <p> Your Score is: ${score}</p>
+  <button onClick="location.reload()">Play Again</button>`;
+
+  endGameEl.style.display = "flex";
+}
+
 addWordToDOM();
 
-// Event Listeners
+// Typing
 text.addEventListener("input", (e) => {
   const insertText = e.target.value;
 
@@ -74,5 +115,25 @@ text.addEventListener("input", (e) => {
 
     // clear
     e.target.value = "";
+
+    if (difficulty === "hard") {
+      time += 2;
+    } else if (difficulty === "medium") {
+      time += 3;
+    } else {
+      time += 5;
+    }
+    updateTime();
   }
+});
+
+//settings button click
+settingBtn.addEventListener("click", () => settings.classList.toggle("hide"));
+
+// setting select
+settingsForm.addEventListener("change", (e) => {
+  difficulty = e.target.value;
+  console.log(difficulty);
+
+  localStorage.setItem("difficulty", difficulty);
 });
