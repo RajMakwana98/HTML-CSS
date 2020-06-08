0ec09de9a013c4fed3d10e9b071ce0f4125cf3b3
Raj Makwana <rajmakwana98@gmail.com>
2020-06-08 14:27:11 +0530

    Added Speech voice
    selected speech voice and text to speech using JS.

--- Speed Text Reader/script.js
+++ Speed Text Reader/script.js
@@ -62,7 +62,71 @@ function createBox(item) {
   box.innerHTML = `<img src="${image}" alt="${text}" />
   <p class="info">${text}</p>`;
 
-  // speak event
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+
+    // add active effect
+    box.classList.add("active");
+    setTimeout(() => box.classList.remove("active"), 800);
+  });
 
   main.appendChild(box);
 }
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
+
+// Store Voices
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// Speak text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+// set voice
+function setVoice(e) {
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+}
+
+// Voices changed
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+// Toggle text box
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+
+// Close Button
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
+
+//change voice
+voicesSelect.addEventListener("change", setVoice);
+
+//Read text button
+readBtn.addEventListener("click", () => {
+  setTextMessage(textArea.value);
+  speakText();
+});
+
+getVoices();
